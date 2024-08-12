a41bbeef7b79089479441e97613a306fe6c1ee5e
Joe Solimini <joe.solimini@underdogfantasy.com>
2024-08-12 13:46:19 -0700

    add luna

--- keyboards/boardsource/unicorne/rules.mk
+++ keyboards/boardsource/unicorne/rules.mk
@@ -1,2 +1,4 @@
 SERIAL_DRIVER = vendor
 POINTING_DEVICE_DRIVER = analog_joystick
+
+WPM_ENABLE = yes
--- keyboards/boardsource/unicorne/unicornehell.c
+++ keyboards/boardsource/unicorne/unicornehell.c
@@ -1,12 +1,128 @@
-// Copyright 2023 jack (@waffle87)
-// SPDX-License-Identifier: GPL-2.0-or-later
-#include "unicorne.h"
+/* Copyright 2021 HellSingCoder
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include QMK_KEYBOARD_H
+
+enum sofle_layers {
+    /* _M_XYZ = Mac Os, _W_XYZ = Win/Linux */
+    _QWERTY,
+    _NUMBERS,
+    _ARROWS,
+};
+
+// clang-format off
+enum custom_keycodes {
+    KC_QWERTY = SAFE_RANGE,
+    KC_GAMING,
+    KC_LOWER,
+    KC_RAISE,
+    KC_ADJUST,
+    KC_PRVWD,
+    KC_NXTWD,
+    KC_LSTRT,
+    KC_LEND,
+    KC_DLINE,
+    KC_BSPC_DEL,
+    KC_LAYER
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /*
+     * QWERTY
+     * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+     * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  `   |
+     * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+     * | Caps |   A  |   S  |   D  |   F  |   G  |                    |   H  |   J  |   K  |   L  |   ;  |  '   |
+     * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+     * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |  \   |
+     * `-----------------------------------------/       /     \      \-----------------------------------------'
+     *                          | LAYER| LCTR | /Enter  /       \Space \  |  [   |  ]   |
+     *                          |      |      |/       /         \      \ |      |      |
+     *                          `---------------------'           '------''-------------'
+     */
+
+[_QWERTY] = LAYOUT(
+            "KC_TAB",  "KC_Q",    "KC_W",    "KC_E",    "KC_R",    "KC_T",          "KC_Y",    "KC_U",    "KC_I",    "KC_O",    "KC_P",    "KC_BSPC",
+            "KC_LCTL", "KC_A",    "KC_S",    "KC_D",    "KC_F",    "KC_G",          "KC_H",    "KC_J",    "KC_K",    "KC_L",    "KC_SCLN", "KC_QUOT",
+            "KC_LSFT", "KC_Z",    "KC_X",    "KC_C",    "KC_V",    "KC_B",          "KC_N",    "KC_M",    "KC_COMM", "KC_DOT",  "KC_SLSH", "KC_ESC",
+                                            "KC_LGUI", "MO(1)",   "KC_ENT",        "KC_SPC",  "MO(2)",   "KC_RALT"
+),
+
+/*
+     * QWERTY
+     * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+     * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  `   |
+     * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+     * | Caps |   A  |   S  |   D  |   F  |   G  |                    |   H  |   J  |   K  |   L  |   ;  |  '   |
+     * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+     * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |  \   |
+     * `-----------------------------------------/       /     \      \-----------------------------------------'
+     *                          | LAYER| LCTR | /Enter  /       \Space \  |  [   |  ]   |
+     *                          |      |      |/       /         \      \ |      |      |
+     *                          `---------------------'           '------''-------------'
+     */
+
+[_NUMBERS] = LAYOUT(
+            "_______", "KC_1",    "KC_2",    "KC_3",    "KC_4",    "KC_5",          "KC_6",    "KC_7",    "KC_8",    "KC_9",    "KC_0",    "_______",
+            "_______", "KC_EXLM", "KC_AT",   "KC_HASH", "KC_DLR",  "KC_PERC",       "KC_CIRC", "KC_AMPR", "KC_ASTR", "KC_LPRN", "KC_RPRN", "_______",
+            "_______", "_______", "_______", "_______", "_______", "_______",       "_______", "_______", "_______", "_______", "_______", "_______",
+                                            "_______", "_______", "_______",       "_______", "_______", "_______"
+),
+
+/*
+     * QWERTY
+     * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+     * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  `   |
+     * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+     * | Caps |   A  |   S  |   D  |   F  |   G  |                    |   H  |   J  |   K  |   L  |   ;  |  '   |
+     * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+     * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |  \   |
+     * `-----------------------------------------/       /     \      \-----------------------------------------'
+     *                          | LAYER| LCTR | /Enter  /       \Space \  |  [   |  ]   |
+     *                          |      |      |/       /         \      \ |      |      |
+     *                          `---------------------'           '------''-------------'
+     */
+
+[_ARROWS] = LAYOUT(
+            "_______", "_______", "_______", "KC_UP", "_______", "_______",       "KC_MINS", "KC_EQL", "RGB_SAI", "RGB_MOD",  "RGB_TOG", "_______",
+            "_______",  "_______", "KC_LEFT", "KC_DOWN", "KC_RIGHT", "_______",       "KC_GRV", "KC_LCBR", "KC_RCBR", "KC_LBRC", "KC_RBRC", "_______",
+            "_______", "_______", "_______", "_______", "_______", "_______",       "KC_BSLS", "KC_LPRN", "KC_RPRN", "KC_LT",  "KC_GT", "_______",
+                                            "_______", "_______", "_______",       "_______", "_______", "_______"
+)
+};
+// clang-format on
 
 #ifdef OLED_ENABLE
-oled_rotation_t oled_init_kb(oled_rotation_t rotation) { return OLED_ROTATION_270; }
+
+/* 32 * 32 logo */
+static void render_logo(void) {
+    static const char PROGMEM hell_logo[] = {0x00, 0x80, 0xc0, 0xc0, 0x60, 0x60, 0x30, 0x30, 0x18, 0x1c, 0x0c, 0x00, 0x00, 0x00, 0x00, 0x80, 0xe0, 0x78, 0x1e, 0x06, 0x00, 0x0c, 0x1c, 0x18, 0x30, 0x30, 0x60, 0x60, 0xc0, 0xc0, 0x80, 0x00, 0x01, 0x03, 0x07, 0x06, 0x0c, 0x0c, 0x18, 0x18, 0x30, 0x70, 0x60, 0x00, 0xc0, 0xf0, 0x3c, 0x0f, 0x03, 0x00, 0x00, 0x00, 0x00, 0x60, 0x70, 0x30, 0x18, 0x18, 0x0c, 0x0c, 0x06, 0x07, 0x03, 0x01, 0x00, 0xf8, 0xf8, 0x80, 0x80, 0x80, 0xf8, 0xf8, 0x00, 0x80, 0xc0, 0xc0, 0x40, 0xc0, 0xc0, 0x80, 0x00, 0xf8, 0xf8, 0x00, 0xf8, 0xf8, 0x00, 0x08, 0x38, 0x08, 0x00, 0x38, 0x08, 0x30, 0x08, 0x38, 0x00, 0x1f, 0x1f, 0x01, 0x01, 0x01, 0x1f, 0x1f, 0x00, 0x0f, 0x1f, 0x1a, 0x12, 0x1a, 0x1b, 0x0b, 0x00, 0x1f, 0x1f, 0x00, 0x1f, 0x1f, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+
+    oled_write_raw_P(hell_logo, sizeof(hell_logo));
+}
 
 /* 32 * 14 os logos */
+static const char PROGMEM windows_logo[] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xbc, 0xbc, 0xbe, 0xbe, 0x00, 0xbe, 0xbe, 0xbf, 0xbf, 0xbf, 0xbf, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x07, 0x07, 0x0f, 0x0f, 0x00, 0x0f, 0x0f, 0x1f, 0x1f, 0x1f, 0x1f, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+
 static const char PROGMEM mac_logo[] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xc0, 0xf0, 0xf8, 0xf8, 0xf8, 0xf0, 0xf6, 0xfb, 0xfb, 0x38, 0x10, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x07, 0x0f, 0x1f, 0x1f, 0x0f, 0x0f, 0x1f, 0x1f, 0x0f, 0x06, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+
+/* Smart Backspace Delete */
+
+bool            shift_held = false;
+static uint16_t held_shift = 0;
 
 /* KEYBOARD PET START */
 
@@ -23,6 +139,7 @@ uint32_t anim_timer = 0;
 
 /* current frame */
 uint8_t current_frame = 0;
+
 /* status variables */
 int   current_wpm = 0;
 led_t led_usb_state;
@@ -145,56 +262,9 @@ static void render_luna(int LUNA_X, int LUNA_Y) {
 
 /* KEYBOARD PET END */
 
-static void print_status_narrow(void) {
-    /* Print current mode */
-    oled_set_cursor(0, 0);
-    oled_write_raw_P(mac_logo, sizeof(mac_logo));
-
-    oled_set_cursor(0, 3);
-
-    switch (get_highest_layer(default_layer_state)) {
-        case 0:
-            oled_write("QWRTY", false);
-            break;
-        case 1:
-            oled_write("NUMS", false);
-            break;
-        case 2:
-            oled_write("MOVES", false);
-            break;
-        default:
-            oled_write("UNDEF", false);
-    }
-
-    oled_set_cursor(0, 5);
-
-    /* Print current layer */
-    oled_write("LAYER", false);
-
-    oled_set_cursor(0, 6);
-
-    switch (get_highest_layer(layer_state)) {
-        case 0:
-            oled_write("Base ", false);
-            break;
-        case 1:
-            oled_write("Nums", false);
-            break;
-        case  2:
-            oled_write("Moves", false);
-            break;
-        default:
-            oled_write("Undef", false);
-    }
-
-    /* KEYBOARD PET RENDER START */
-
-    render_luna(0, 13);
-
-    /* KEYBOARD PET RENDER END */
-}
-
 static void print_logo_narrow(void) {
+    render_logo();
+
     /* wpm counter */
     uint8_t n = get_current_wpm();
     char    wpm_str[4];
@@ -209,8 +279,68 @@ static void print_logo_narrow(void) {
     oled_write(" wpm", false);
 }
 
-bool oled_task_kb(void) {
+static void print_status_narrow(void) {
+    /* Print current mode */
+    oled_set_cursor(0, 0);
+    if (keymap_config.swap_lctl_lgui) {
+        oled_write_raw_P(mac_logo, sizeof(mac_logo));
+    } else {
+        oled_write_raw_P(windows_logo, sizeof(windows_logo));
+    }
+
+    oled_set_cursor(0, 3);
+
+    switch (get_highest_layer(default_layer_state)) {
+        case _QWERTY:
+            oled_write("QWRTY", false);
+            break;
+        case _NUMBERS:
+            oled_write("NUMBERS", false);
+            break;
+        case _ARROWS:
+            oled_write("ARROWS", false);
+            break;
+        default:
+            oled_write("UNDEF", false);
+    }
+
+    oled_set_cursor(0, 5);
+
+    /* Print current layer */
+    oled_write("LAYER", false);
+
+    oled_set_cursor(0, 6);
+
+    switch (get_highest_layer(layer_state)) {
+        case _QWERTY:
+            oled_write("Base ", false);
+            break;
+        case _NUMBERS:
+            oled_write("Numbers", false);
+            break;
+        case  _ARROWS:
+            oled_write("Arrows", false);
+            break;
+        default:
+            oled_write("Undef", false);
+    }
+
+    /* caps lock */
+    oled_set_cursor(0, 8);
+    oled_write("CPSLK", led_usb_state.caps_lock);
+
+    /* KEYBOARD PET RENDER START */
+
+    render_luna(0, 13);
+
+    /* KEYBOARD PET RENDER END */
+}
+
+oled_rotation_t oled_init_user(oled_rotation_t rotation) { return OLED_ROTATION_270; }
+
+bool oled_task_user(void) {
     /* KEYBOARD PET VARIABLES START */
+
     current_wpm   = get_current_wpm();
     led_usb_state = host_keyboard_led_state();
 
@@ -251,3 +381,40 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     }
     return true;
 }
+
+#ifdef ENCODER_ENABLE
+
+bool encoder_update_user(uint8_t index, bool clockwise) {
+    if (index == 0) {
+        if (clockwise) {
+            if (shift_held) {
+                tap_code(KC_MNXT);
+            } else {
+                tap_code(KC_RIGHT);
+            }
+        } else {
+            if (shift_held) {
+                tap_code(KC_MPRV);
+            } else {
+                tap_code(KC_LEFT);
+            }
+        }
+    } else if (index == 1) {
+        if (clockwise) {
+            if (shift_held) {
+                tap_code(KC_VOLU);
+            } else {
+                tap_code(KC_DOWN);
+            }
+        } else {
+            if (shift_held) {
+                tap_code(KC_VOLD);
+            } else {
+                tap_code(KC_UP);
+            }
+        }
+    }
+    return true;
+}
+
+#endif
